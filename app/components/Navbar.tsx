04fe916332ec4d3b0060053dc7a2bd0ab01b1676
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2024-01-02 12:22:51 +0100

    [NEW] FInalizada la barra de navegacion

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,6 +2,8 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { ShoppingBagIcon } from "lucide-react";
 
 const links = [
   {name: 'Home',  href: '/'},
@@ -16,7 +18,7 @@ export default function Navbar() {
     <header className="mb-8 border-b">
       <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
         <Link href="/">
-          <h1 className="text-4xl font-bold">
+          <h1 className="text-2xl md:text-4xl font-bold">
             Next<span className="text-primary">Commerce</span>
           </h1>
         </Link>
@@ -37,7 +39,12 @@ export default function Navbar() {
         ))}
       </nav>
       <div className="flex divide-x border-r sm:border-l">
-              <button></button>
+        <Button variant={"outline"} className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none">
+          <ShoppingBagIcon />
+          <span className=" hiiden text-xs font-semibold text-gray-500 sm:block">
+              Cart
+          </span>
+        </Button>
       </div>
       </div>
     </header>
